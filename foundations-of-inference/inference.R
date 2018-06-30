0331b5d07344bb71e0c123c98b2b480ddf3af568
Cássio Jandir Pagnoncelli <cassiopagnoncelli@gmail.com>
2018-06-30 02:34:43 -0300

    last chapter

--- foundations-of-inference/inference.R
+++ foundations-of-inference/inference.R
@@ -27,6 +27,7 @@ library('dplyr')
 library('ggplot2')
 library('NHANES')
 library('infer')
+library('oilabs')
 
 data('NHANES')
 
@@ -191,3 +192,166 @@ homeown_perm %>%
 # 
 # Because 0.03 < 0.05 we REJECT H_0 in favour of H_a.
 # 
+
+# Hypothesis testing errors: opportunity cost.
+# 
+# Two control groups of students are each given a set of
+# options about buying something.
+# 
+#   Group 1: (1) buy, or (2) not buy.
+#   Group 2: (1) buy, or (2) not buy, save $20 for next purchase.
+# 
+# Then we get the conversion rates (prop_buy) for each group. We
+# want to know whether there is a difference in conversion rates
+# by using messaging in Group 2, or messaging in Group 1 is better.
+# 
+# Here we have hypothesis
+# 
+#   H_0: Difference in conversion rates is zero.
+#   H_a: Difference in conversion rates is not zero.
+# 
+# In other words,
+# 
+#   H_0: Reminding students that they can save money for later
+#        purchases will not have any impact on students' spending
+#        decisions.
+#   H_a: Reminding students that they can save money for later
+#        purchases will change the chance they will continue with
+#        a purchase.
+#
+opportunity = read.csv("foundations-of-inference/opportunity.csv", header=T)
+
+opp_perm <- opportunity %>%
+  rep_sample_n(size = nrow(opportunity), reps = 1000) %>%
+  mutate(dec_perm = sample(decision)) %>%
+  group_by(replicate, group) %>%
+  summarize(prop_buy_perm = mean(dec_perm == "buyDVD"),
+            prop_buy = mean(decision == "buyDVD")) %>%
+  summarize(diff_perm = diff(prop_buy_perm),
+            diff_orig = diff(prop_buy))  # treatment - control
+opp_perm
+
+ggplot(opp_perm, aes(x = diff_perm)) + 
+  geom_histogram(binwidth = .005) +
+  geom_vline(aes(xintercept = diff_orig), col = "red")
+
+# p-value, or the proportion of permuted differences less than or
+# equal to the observed difference.
+# 
+# Conclusion is we can confidently say the different messaging
+# caused the students to change their buying habits, since they
+# were randomly assigned to treatment and control groups.
+# 
+opp_perm %>% summarize(onesided.pvalue = mean(diff_perm <= diff_orig))
+
+opp_perm %>% summarize(twosided.pvalue = 2 * mean(diff_perm <= diff_orig))
+
+# ERRORS IN HYPOTHESIS TESTING.
+# 
+#                 +-----------------------+-------------------+
+#                 |   Don't reject H_0    |     Reject H_0    |
+# +---------------|-----------------------+-------------------+
+# | H_0 is true   |                       |    type I error   |
+# |               |                       |                   |
+# | H_a is true   |    type II error      |                   |
+# +---------------+-----------------------+-------------------+
+# 
+# type II error: false negative
+# type I error: false positive
+# 
+# Here, if you always claim there is a difference in proportions,
+# you'll always reject the null hypothesis, so you'll only make
+# type I errors, if any.
+# In other words, always rejecting H_0 will cease type II errors.
+# 
+# Moreover,
+# 
+# Type I: There is a difference in proportions, and the observed
+# difference is big enough to indicate that the proportions are
+# different.
+# 
+# Type II: There is not a difference in proportions, and the
+# observed difference is not large enough to indicate that the
+# proportions are different.
+# 
+
+
+
+# PARAMETERS AND CONFIDENCE INTERVALS.
+# 
+# Research questions examples.
+# 
+# Hypothesis test (comparative): 
+# - under which diet plan will participants lose more weight on avg?
+# - which of two car manufacturers more likely to recommend to friends?
+# - are education level and avg income linearly related?
+# 
+# Confidence interval (estimation):
+# - how much should participants expect to lose on avg?
+# - what % of users are likely to recommend Subaru to friends?
+# - for each additional year of education what is predicted income?
+# 
+# 
+# Confidence interval.
+# "We are 95% sure that between 12% and 34% of US population
+# recommends Subaru."
+# 
+# The population parameter has to do with *all* the population.
+#
+
+
+# BOOTSTRAPPING: resampling with replacement.
+# 
+# Variability of p-hat from the population.
+# 
+# Typically the researcher has only one sample from the population.
+# Turns out bootstrapping several re-samples (with repetition, 
+# of course) from the sample yields awesome results.
+# Actually, we know the size of the sample we need to have to give
+# a certain standard deviation.
+# 
+# Goal here is to find the parameter when all we have is the statistic,
+# never knowing whether the sample really contains the true parameter.
+# 
+# Technical conditions apply:
+# - Sampling distribution of the statistic is reasonably symmetric and
+#   bell-shaped.
+# - Sample size is reasonably large.
+# 
+
+samp = data.frame(a = rbinom(50, size = 100, prob = 0.3))
+head(samp)
+
+samp_boot = samp %>% rep_sample_n(10, replace = T, reps = 5000)
+head(samp_boot)
+
+props = samp_boot %>% summarize(prop_yes = mean(a)) # p-hat.
+head(props)
+
+props %>% summarize(sd(prop_yes)) # sd of p-hat.
+
+# Distribution of p-hat.
+# 
+# Approximately 95% of samples will produce p-hats within ±2 sd,
+# in other words 95% confident that the true parameter is within
+# the calculated confidence interval.
+# 
+ggplot() + 
+  geom_density(
+    data = props, 
+    aes(x = prop_yes), 
+    col = "blue", 
+    bw = .1
+  )
+
+props %>%
+  mutate(lower = 30 - 2 * sd(prop_yes),
+         upper = 30 + 2 * sd(prop_yes),
+         in_CI = prop_yes > lower & prop_yes < upper) %>%
+  summarize(mean(in_CI))
+
+props %>% 
+  summarize(
+    q025 = quantile(prop_yes, 0.025),
+    q925 = quantile(prop_yes, 0.975)
+  )
